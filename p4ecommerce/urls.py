9f037da6f4a82fef673dde8c29262565593499ba
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-10-31 12:22:33 -0700

    update

--- p4ecommerce/urls.py
+++ p4ecommerce/urls.py
@@ -20,13 +20,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('securelogin/', admin.site.urls),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
-    path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
-
-    # ORDERS
+    path('cart/', include('carts.urls')),
     path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
