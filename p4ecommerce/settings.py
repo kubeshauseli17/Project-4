3328cada1013c8d5c166c29e6058744551a49489
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-11-14 21:09:50 -0800

    restore before aws

--- p4ecommerce/settings.py
+++ p4ecommerce/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = [
-'django-env.eba-qmhvxhud.us-west-2.elasticbeanstalk.com',
-'127.0.0.1',
-]
+ALLOWED_HOSTS = []
 
 # Application definition
 
