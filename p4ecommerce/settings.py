6b309f54248080ba4d1405db8ef4428aaf6c25cf
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-12-13 15:44:33 -0800

    make it better

--- p4ecommerce/settings.py
+++ p4ecommerce/settings.py
@@ -188,7 +188,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # media files configuration
 MEDIA_URL = '/media/'
