92b6beaa5832315d39077016a4a7efad5d72fa39
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-12-13 17:10:26 -0800

    make it better

--- p4ecommerce/settings.py
+++ p4ecommerce/settings.py
@@ -186,7 +186,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = "static"
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WHITENOISE_USE_FINDERS = True
 
