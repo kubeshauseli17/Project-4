92669422c2c1781a0ab3839a7905d0d9d19d504e
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-12-13 18:18:37 -0800

    make it better

--- p4ecommerce/settings.py
+++ p4ecommerce/settings.py
@@ -187,7 +187,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = "os.path.join(BASE_DIR, 'staticfiles')"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static/public/images')
 
 WHITENOISE_USE_FINDERS = True
 
