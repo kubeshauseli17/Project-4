23fcecc4f14143dc42cc034510d6b43466a0baa5
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-12-15 12:24:21 -0800

    upd

--- p4ecommerce/settings.py
+++ p4ecommerce/settings.py
@@ -168,7 +168,7 @@ MEDIA_ROOT = BASE_DIR /'media'
 
 # SMTP configuration
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = os.environ.get('EMAIL_PORT', cast=int)
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', cast=bool)
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
