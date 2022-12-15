dc0488bc3441f79d0f06e7c53779bb152c2d07f0
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-12-15 00:01:26 -0800

    final polishes

--- p4ecommerce/settings.py
+++ p4ecommerce/settings.py
@@ -162,6 +162,10 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = 'public-read'
 
+# media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 
 # SMTP configuration
 EMAIL_HOST = config('EMAIL_HOST')
